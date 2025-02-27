1b9ee272468f48ecc78d1bfec64ea25d4fe91ffb
Rdani2005 <daniel.sequeira@betanysports.eu>
2025-02-27 08:12:05 -0600

    feat: adding dragonball page

--- bases/src/app/app.routes.ts
+++ bases/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page/hero-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page/dragonball-page.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'heroes',
     component: HeroPageComponent,
+  },
+  {
+    path: 'dragonball',
+    component: DragonballPageComponent,
   },
 ];
