0ce6f8d208e568c2fc7d3b0cc44f9022a33e5291
Rdani2005 <daniel.sequeira@betanysports.eu>
2025-03-03 08:09:49 -0600

    i dont know

--- bases/src/app/pages/dragonball/dragonball-page/dragonball-page.component.ts
+++ bases/src/app/pages/dragonball/dragonball-page/dragonball-page.component.ts
@@ -1,11 +1,22 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component, signal } from '@angular/core';
+
+type Character = {
+  id: number;
+  name: string;
+  power: number;
+};
 
 @Component({
   selector: 'app-dragonball-page',
-  imports: [],
+  imports: [NgClass, NgFor],
   templateUrl: './dragonball-page.component.html',
   styleUrl: './dragonball-page.component.css',
 })
 export class DragonballPageComponent {
-  characters = signal([]);
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9000 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 3, name: 'Gohan', power: 7000 },
+  ]);
 }
